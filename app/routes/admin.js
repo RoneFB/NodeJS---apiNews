70b5eedf33f624d7a3d503e91579f5c1f12275d4
RoneFB <rone.bento@fatec.sp.gov.br>
2019-12-28 19:26:47 -0300

    body-parser include

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -2,4 +2,10 @@ module.exports = function(app){
     app.get('/formulario_inclusao_noticia', function(req, resp){
         resp.render("admin/form_add_noticia")
     })
+    
+    app.post('/noticias/salvar', function(req, resp){
+        var noticias = req.body;
+
+        resp.send(noticias)
+    })
 };
--- config/sever.js
+++ config/sever.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var consign = require('consign');
+var bodyParser = require('body-parser');
 
 var app = express();
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
+
+/**midleware */
+app.use(bodyParser.urlencoded({extended: true}));
 
 /* inclusão das rotas no consign e execução dentro do servidor */
 consign()
