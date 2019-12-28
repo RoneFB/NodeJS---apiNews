6e147e8c99255ba827b5b2c6b676854dffdd11b7
RoneFB <rone.bento@fatec.sp.gov.br>
2019-12-28 13:34:43 -0300

    Instalando Consign para detectar rotas e autoload

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var app = require('./config/sever');
 
-require('./app/routes/noticias')(app);
-require('./app/routes/home')(app);
-require('./app/routes/formulario_inclusao_noticia')(app);
+// require('./app/routes/noticias')(app); Rotas estão sendo consideradas pelo Consign
+// require('./app/routes/home')(app);
+// require('./app/routes/formulario_inclusao_noticia')(app);
 
 app.listen(3000, function(){
     console.log('Servidor On');
--- config/sever.js
+++ config/sever.js
@@ -1,6 +1,11 @@
 var express = require('express');
+var consign = require('consign');
+
 var app = express();
 app.set('view engine', 'ejs');
-app.set('views', './app/views')
+app.set('views', './app/views');
+
+/* inclusão das rotas no consign e execução dentro do servidor */
+consign().include('app/routes').into(app);
 
 module.exports = app;
