f06a658fa45b3a56eaf7b79cc95e92c485363f4b
RoneFB <rone.bento@fatec.sp.gov.br>
2019-12-28 13:17:59 -0300

    modularizando a conexão

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,19 +1,13 @@
+var dbConnection = require('../../config/dbConnection');
+
 module.exports = function(app){
 
     app.get('/noticias', function(req, resp){  
-     
-      var mysql = require('mysql');
-      var conn  = mysql.createConnection({
-        host : '127.0.0.1',
-        user : 'root',
-        password : '',
-        database : 'portal_noticias'
-      });
-
+   
+      var conn = dbConnection();
       conn.query('select * from noticias', function(error, result){
           resp.render("noticias/noticias", {noticias: result});
       });
-
      
     });
 };
