ffd922f1917db0e43218e849b69d113f4c4d859b
RoneFB <rone.bento@fatec.sp.gov.br>
2020-01-13 21:06:55 -0300

    include

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -6,6 +6,19 @@ module.exports = function(application){
     application.post('/noticias/salvar', function(req, resp){
         var noticia = req.body;
 
+        req.assert('titulo', 'Titulo é obrigatório').notEmpty();
+        req.assert('resume', 'Resumo é obrigatório').notEmpty();
+        req.assert('resume', 'Resumo deve conter entre 10 e 100 caracteres').len(10,100);
+        req.assert('autor', 'Resumo é obrigatório').notEmpty();
+        req.assert('data_noticia', 'Data é obrigatório').notEmpty().isDate({format: 'YYYY-MM-DD'});
+        req.assert('noticia', 'Noticia é obrigatório').notEmpty();
+       
+       var erros = req.validationErrors();
+        console.log(erros); 
+        if(erros){
+            res.render('admin/form_add_noticia', {validacao: erros,  noticia: noticia});
+            return;
+        }
         //save
         var conn = application.config.dbConnection();
         var NoticiasDAO = new application.app.models.NoticiasDAO(conn);
--- config/sever.js
+++ config/sever.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 var app = express();
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
-/**midleware */
+/**middleware */
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressValidator());
 
 /* inclusão das rotas no consign e execução dentro do servidor */
 consign()
